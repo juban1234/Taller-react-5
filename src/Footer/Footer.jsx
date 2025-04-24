d6b32ebc17778218fc80e7f0435ef01a09de74ef
juan esteban grajales <alexzo8677@gmail.com>
2025-04-24 14:54:32 -0500

    actualizacion para poner el footer

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="bg-black text-white text-center py-4 mt-8">
+      <p>Creado por Tu Nombre • Abril 2025</p>
+    </footer>
+  );
+};
+
